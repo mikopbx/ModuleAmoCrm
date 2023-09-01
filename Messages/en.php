a3d9435d40a37f6005fc153bd146c83cd29deed5
Weblate <noreply@weblate.org>
2023-09-01 15:11:34 +0300

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MIKOPBX/ModuleAmoCrm
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleamocrm/

--- Messages/en.php
+++ Messages/en.php
@@ -1,38 +1,35 @@
 <?php
-/**
+return [
+    /**
  * Copyright (C) MIKO LLC - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Nikolay Beketov, 6 2018
  *
  */
-
-return [
-	'repModuleAmoCrm'       => 'Module amoCRM - %repesent%',
-	'mo_ModuleModuleAmoCrm' => 'Module amoCRM',
-    'BreadcrumbModuleAmoCrm'=> 'Module amoCRM',
+    'repModuleAmoCrm' => 'Module amoCRM - %repesent%',
+    'mo_ModuleModuleAmoCrm' => 'Module amoCRM',
+    'BreadcrumbModuleAmoCrm' => 'Module amoCRM',
     'SubHeaderModuleAmoCrm' => 'Example to create own modules',
-    'mod_amo_authCodeText'          => 'Enter auth code',
-    'module_amo_crmSimpleLogin'     => 'Enter auth code',
-    'mod_amo_authCodeSave'          => 'Save',
-    'module_amo_crm_connect_ok'     => 'Connected',
-    'module_amo_crm_connect_fail'   => 'Disconnected',
-
-    'mod_amo_rules'                 => 'Rules for creating entities',
-    'mod_amo_settingsConnection'    => 'Connection Settings',
-    'mod_amo_baseDomain'            => 'Portal AMO (example.amocrm.ru)',
-    'mod_amo_Error'                 => 'Error',
-    'module_template_AddNewRecord'  => 'Add new',
-    'module_amo_crmTextFieldLabel'        => 'Text field example',
-    'module_amo_crmTextAreaFieldLabel'    => 'TextArea field example',
-    'module_amo_crmPasswordFieldLabel'    => 'Password field example',
-    'module_amo_crmIntegerFieldLabel'     => 'Integer field example',
-    'module_amo_crmCheckBoxFieldLabel'    => 'CheckBox',
-    'module_amo_crmToggleFieldLabel'      => 'Toggle',
-    'module_amo_crmDropDownFieldLabel'    => 'Dropdown menu',
-    'module_amo_crmValidateValueIsEmpty'  => 'Check the field, it looks like empty',
-    'module_amo_crmConnected'             => 'Module connected',
-    'module_amo_crmDisconnected'          => 'Module disconnected',
-    'module_amo_crmUpdateStatus'          => 'Update module status',
-    'mod_amo_tokenForAmo'                 => 'Token API MikoPBX',
+    'mod_amo_authCodeText' => 'Enter auth code',
+    'module_amo_crmSimpleLogin' => 'Enter auth code',
+    'mod_amo_authCodeSave' => 'Save',
+    'module_amo_crm_connect_ok' => 'Connected',
+    'module_amo_crm_connect_fail' => 'Disconnected',
+    'mod_amo_rules' => 'Rules for creating entities',
+    'mod_amo_settingsConnection' => 'Connection Settings',
+    'mod_amo_baseDomain' => 'Portal AMO (example.amocrm.ru)',
+    'mod_amo_Error' => 'Error',
+    'module_amo_crmTextFieldLabel' => 'Text field example',
+    'module_amo_crmTextAreaFieldLabel' => 'TextArea field example',
+    'module_amo_crmPasswordFieldLabel' => 'Password field example',
+    'module_amo_crmIntegerFieldLabel' => 'Integer field example',
+    'module_amo_crmCheckBoxFieldLabel' => 'CheckBox',
+    'module_amo_crmToggleFieldLabel' => 'Toggle',
+    'module_amo_crmDropDownFieldLabel' => 'Dropdown menu',
+    'module_amo_crmValidateValueIsEmpty' => 'Check the field, it looks like empty',
+    'module_amo_crmConnected' => 'Module connected',
+    'module_amo_crmDisconnected' => 'Module disconnected',
+    'module_amo_crmUpdateStatus' => 'Update module status',
+    'mod_amo_tokenForAmo' => 'Token API MikoPBX',
 ];
