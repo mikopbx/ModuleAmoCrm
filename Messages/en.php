10df67f2bf0018c876e7fcbf68701b4870f5d9ea
Anonymous <noreply@weblate.org>
2023-10-23 10:19:54 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (75 of 75 strings)
    
    Translation: MIKOPBX/ModuleAmoCrm
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleamocrm/en/

--- Messages/en.php
+++ Messages/en.php
@@ -81,4 +81,5 @@ return [
     'mod_amo_type_responsibleVarFirst' => 'The first person to answer the call',
     'mod_amo_type_responsibleVarLast' => 'The last person to answer the call',
     'mod_amo_rule_for_type_did_exists' => 'The rule for this external number (DID) and this type of call has already been described earlier. Duplicate creation is prohibited.',
+    'mod_amo_SaveSettingsError' => 'An error occurred while saving settings. The ConnectorDB service may not be running.',
 ];
