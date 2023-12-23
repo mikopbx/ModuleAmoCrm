93790cc99a4eaf67329e2417892c305a352e59a6
Anonymous <noreply@weblate.org>
2023-12-23 04:45:04 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (78 of 78 strings)
    
    Translation: MIKOPBX/ModuleAmoCrm
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleamocrm/ja/

--- Messages/ja.php
+++ Messages/ja.php
@@ -84,4 +84,5 @@ return [
     'mod_amo_SaveSettingsError' => '設定の保存中にエラーが発生しました。 ConnectorDB サービスが実行されていない可能性があります。',
     'mod_amo_NeedWaitSyncTitle' => '連絡先を同期する',
     'mod_amo_NeedWaitSyncBody' => '連絡先とトランザクションは同期されます。プロセスが完了するまで待ちます。ポータルへの通話のアップロードが停止しました。',
+    'mod_amo_panelIsEnable' => '通話コントロールパネルを使用する',
 ];
