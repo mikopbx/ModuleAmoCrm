4b1583934b587fe82e18a2e8053d463fe855bea6
Nikolay Beketov <nbek@miko.ru>
2024-10-16 11:17:46 +0700

    Refactor checkbox usage
    Replaced redundant checkbox code with a new `addCheckBox` method across multiple forms for consistency and maintainability. Updated relevant dependencies and forms to ensure proper integration, and fixed minor issues in JavaScript files.

--- App/Forms/ModuleAmoCrmEntitySettingsModifyForm.php
+++ App/Forms/ModuleAmoCrmEntitySettingsModifyForm.php
@@ -1,30 +1,40 @@
 <?php
-/**
- * Copyright (C) MIKO LLC - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Nikolay Beketov, 9 2018
+
+/*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2024 Alexey Portnov and Nikolay Beketov
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
+
 namespace Modules\ModuleAmoCrm\App\Forms;
 
 use MikoPBX\Common\Models\Extensions;
 use MikoPBX\Core\System\Util;
 use Modules\ModuleAmoCrm\bin\AmoCdrDaemon;
 use Modules\ModuleAmoCrm\bin\ConnectorDb;
-use Phalcon\Forms\Element\Check;
 use Phalcon\Forms\Element\Numeric;
-use Phalcon\Forms\Form;
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Element\Hidden;
 use Phalcon\Forms\Element\Select;
 
-class ModuleAmoCrmEntitySettingsModifyForm extends Form
+class ModuleAmoCrmEntitySettingsModifyForm extends ModuleBaseForm
 {
-    public function initialize($entity = null, $options = null) :void
+    public function initialize($entity = null, $options = null): void
     {
-        $this->add(new Hidden('id',             ['value' => $entity->id]));
-        $this->add(new Hidden('portalId',       ['value' => $entity->portalId]));
+        $this->add(new Hidden('id', ['value' => $entity->id]));
+        $this->add(new Hidden('portalId', ['value' => $entity->portalId]));
         $this->add(new Text('did'));
 
         $this->add(new Text('template_contact_name'));
@@ -32,41 +42,24 @@ class ModuleAmoCrmEntitySettingsModifyForm extends Form
         $this->add(new Text('template_task_text'));
         $this->add(new Numeric('deadline_task', ["maxlength" => 3, "style" => "width: 80px;"]));
 
-        $values = ['value' => null];
-        if ($entity->create_contact === '1') {
-            $values = ['checked' => 'checked', 'value' => null];
-        }
-        $this->add(new Check('create_contact', $values));
-
-        $values = ['value' => null];
-        if ($entity->create_lead === '1') {
-            $values = ['checked' => 'checked', 'value' => null];
-        }
-        $this->add(new Check('create_lead', $values));
-
-        $values = ['value' => null];
-        if ($entity->create_unsorted === '1') {
-            $values = ['checked' => 'checked', 'value' => null];
-        }
-        $this->add(new Check('create_unsorted', $values));
-
-        $values = ['value' => null];
-        if ($entity->create_task === '1') {
-            $values = ['checked' => 'checked', 'value' => null];
-        }
-        $this->add(new Check('create_task', $values));
+        $this->addCheckBox('create_contact', intval($entity->create_contact) === 1);
+        $this->addCheckBox('create_lead', intval($entity->create_lead) === 1);
+        $this->addCheckBox('create_unsorted', intval($entity->create_unsorted) === 1);
+        $this->addCheckBox('create_task', intval($entity->create_task) === 1);
 
         $types = [
-            AmoCdrDaemon::INCOMING_UNKNOWN      => Util::translate('mod_amo_type_'.AmoCdrDaemon::INCOMING_UNKNOWN, false),
-            AmoCdrDaemon::INCOMING_KNOWN        => Util::translate('mod_amo_type_'.AmoCdrDaemon::INCOMING_KNOWN, false),
-            AmoCdrDaemon::MISSING_UNKNOWN       => Util::translate('mod_amo_type_'.AmoCdrDaemon::MISSING_UNKNOWN, false),
-            AmoCdrDaemon::MISSING_KNOWN         => Util::translate('mod_amo_type_'.AmoCdrDaemon::MISSING_KNOWN, false),
-            AmoCdrDaemon::OUTGOING_UNKNOWN      => Util::translate('mod_amo_type_'.AmoCdrDaemon::OUTGOING_UNKNOWN, false),
-            AmoCdrDaemon::OUTGOING_KNOWN_FAIL   => Util::translate('mod_amo_type_'.AmoCdrDaemon::OUTGOING_KNOWN_FAIL, false),
-            AmoCdrDaemon::OUTGOING_KNOWN        => Util::translate('mod_amo_type_'.AmoCdrDaemon::OUTGOING_KNOWN, false),
+            AmoCdrDaemon::INCOMING_UNKNOWN      => Util::translate('mod_amo_type_' . AmoCdrDaemon::INCOMING_UNKNOWN, false),
+            AmoCdrDaemon::INCOMING_KNOWN        => Util::translate('mod_amo_type_' . AmoCdrDaemon::INCOMING_KNOWN, false),
+            AmoCdrDaemon::MISSING_UNKNOWN       => Util::translate('mod_amo_type_' . AmoCdrDaemon::MISSING_UNKNOWN, false),
+            AmoCdrDaemon::MISSING_KNOWN         => Util::translate('mod_amo_type_' . AmoCdrDaemon::MISSING_KNOWN, false),
+            AmoCdrDaemon::OUTGOING_UNKNOWN      => Util::translate('mod_amo_type_' . AmoCdrDaemon::OUTGOING_UNKNOWN, false),
+            AmoCdrDaemon::OUTGOING_KNOWN_FAIL   => Util::translate('mod_amo_type_' . AmoCdrDaemon::OUTGOING_KNOWN_FAIL, false),
+            AmoCdrDaemon::OUTGOING_KNOWN        => Util::translate('mod_amo_type_' . AmoCdrDaemon::OUTGOING_KNOWN, false),
         ];
         $type = new Select(
-            'type', $types, [
+            'type',
+            $types,
+            [
                   'useEmpty' => false,
                   'class' => 'ui selection dropdown search',
                   'using' => [
@@ -79,10 +72,12 @@ class ModuleAmoCrmEntitySettingsModifyForm extends Form
 
         $responsibleVar = [
             'first'      => Util::translate('mod_amo_type_responsibleVarFirst', false),
-            'last'       => Util::translate('mod_amo_type_responsibleVarLast',  false),
+            'last'       => Util::translate('mod_amo_type_responsibleVarLast', false),
         ];
         $type = new Select(
-            'responsible', $responsibleVar, [
+            'responsible',
+            $responsibleVar,
+            [
                   'useEmpty' => false,
                   'class' => 'ui selection dropdown search',
                 ]
@@ -92,20 +87,20 @@ class ModuleAmoCrmEntitySettingsModifyForm extends Form
         $pipeLinesData    = ConnectorDb::invoke('getPipeLines', [true]);
         $pipeLineStatuses = [];
         $pipeLinesList    = [];
-        foreach ($pipeLinesData as $pipeLine){
+        foreach ($pipeLinesData as $pipeLine) {
             $pipeLinesList[$pipeLine['amoId']] = $pipeLine['name'];
             try {
                 $statuses = json_decode($pipeLine['statuses'], true, 512, JSON_THROW_ON_ERROR);
-                foreach ($statuses as $index => $status){
-                    if(in_array($status['id'], [142,143,51569353], true)){
+                foreach ($statuses as $index => $status) {
+                    if (in_array($status['id'], [142,143,51569353], true)) {
                         continue;
                     }
-                    if($index === 0){
+                    if ($index === 0) {
                         continue;
                     }
-                    if($pipeLine['amoId'] === $entity->lead_pipeline_id){
-                        $selected = $entity->lead_pipeline_status_id===(string)$status['id'];
-                    }else{
+                    if ($pipeLine['amoId'] === $entity->lead_pipeline_id) {
+                        $selected = $entity->lead_pipeline_status_id === (string)$status['id'];
+                    } else {
                         $selected = $index === 1;
                     }
                     $pipeLineStatuses[$pipeLine['amoId']][] = [
@@ -114,13 +109,15 @@ class ModuleAmoCrmEntitySettingsModifyForm extends Form
                         'selected' => $selected,
                     ];
                 }
-            }catch (\Throwable $e){
+            } catch (\Throwable $e) {
                 continue;
             }
         }
-        $this->add(new Hidden('pipeLineStatuses',             ['value' => json_encode($pipeLineStatuses)]));
+        $this->add(new Hidden('pipeLineStatuses', ['value' => json_encode($pipeLineStatuses)]));
         $lead_pipeline_id = new Select(
-            'lead_pipeline_id', $pipeLinesList, [
+            'lead_pipeline_id',
+            $pipeLinesList,
+            [
                              'useEmpty' => false,
                              'class' => 'ui selection dropdown search',
                          ]
@@ -128,7 +125,9 @@ class ModuleAmoCrmEntitySettingsModifyForm extends Form
         $this->add($lead_pipeline_id);
 
         $lead_pipeline_status_id = new Select(
-            'lead_pipeline_status_id', [], [
+            'lead_pipeline_status_id',
+            [],
+            [
                              'useEmpty' => false,
                              'class' => 'ui selection dropdown search',
                          ]
@@ -136,28 +135,32 @@ class ModuleAmoCrmEntitySettingsModifyForm extends Form
         $this->add($lead_pipeline_status_id);
 
 
-        $extensions = Extensions::find("type='".Extensions::TYPE_SIP."'");
+        $extensions = Extensions::find("type='" . Extensions::TYPE_SIP . "'");
         $pbxUsers = [];
-        foreach ($extensions as $extension){
+        foreach ($extensions as $extension) {
             $pbxUsers[$extension->number] = $extension->callerid;
         }
         unset($extensions);
 
         $usersAmo = ConnectorDb::invoke('getPortalUsers', [1]);
         $users    = [];
-        foreach ($usersAmo as $user){
-            $users[$user['amoUserId']] = ($pbxUsers[$user['number']]??''). " <{$user['number']}>";
+        foreach ($usersAmo as $user) {
+            $users[$user['amoUserId']] = ($pbxUsers[$user['number']] ?? '') . " <{$user['number']}>";
         }
 
         $def_responsible = new Select(
-            'def_responsible', $users, [
+            'def_responsible',
+            $users,
+            [
                                          'useEmpty' => false,
                                          'class' => 'ui selection dropdown search',
                                      ]
         );
         $this->add($def_responsible);
         $entityAction = new Select(
-            'entityAction', [], [
+            'entityAction',
+            [],
+            [
                                  'useEmpty' => false,
                                  'class' => 'ui selection dropdown search',
                              ]
@@ -165,10 +168,12 @@ class ModuleAmoCrmEntitySettingsModifyForm extends Form
         $this->add($entityAction);
 
         $taskResponsibleType = [
-            $entity->task_responsible_type => Util::translate('mod_amo_task_responsible_type_'.$entity->task_responsible_type)
+            $entity->task_responsible_type => Util::translate('mod_amo_task_responsible_type_' . $entity->task_responsible_type)
         ];
         $taskResponsibleTypeAction = new Select(
-            'task_responsible_type', $taskResponsibleType, [
+            'task_responsible_type',
+            $taskResponsibleType,
+            [
                                  'useEmpty' => false,
                                  'class' => 'ui selection dropdown search',
                              ]
--- App/Forms/ModuleAmoCrmForm.php
+++ App/Forms/ModuleAmoCrmForm.php
@@ -1,69 +1,58 @@
 <?php
-/**
- * Copyright (C) MIKO LLC - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Nikolay Beketov, 9 2018
+
+/*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2024 Alexey Portnov and Nikolay Beketov
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
+
 namespace Modules\ModuleAmoCrm\App\Forms;
 
 use MikoPBX\Core\System\Util;
 use Modules\ModuleAmoCrm\Lib\AmoCrmMainBase;
 use Modules\ModuleAmoCrm\Models\ModuleAmoCrm;
-use Phalcon\Forms\Element\Check;
 use Phalcon\Forms\Element\Select;
-use Phalcon\Forms\Form;
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Element\Hidden;
 
-
-class ModuleAmoCrmForm extends Form
+class ModuleAmoCrmForm extends ModuleBaseForm
 {
-    public function initialize($entity = null, $options = null) :void
+    public function initialize($entity = null, $options = null): void
     {
-        $this->add(new Hidden('id',             ['value' => $entity->id]));
-        $this->add(new Hidden('referenceDate',  ['value' => $entity->id]));
-        $this->add(new Hidden('clientId',       ['value' => AmoCrmMainBase::CLIENT_ID]));
-        $this->add(new Hidden('redirectUri',    ['value' => AmoCrmMainBase::REDIRECT_URL]));
+        $this->add(new Hidden('id', ['value' => $entity->id]));
+        $this->add(new Hidden('referenceDate', ['value' => $entity->id]));
+        $this->add(new Hidden('clientId', ['value' => AmoCrmMainBase::CLIENT_ID]));
+        $this->add(new Hidden('redirectUri', ['value' => AmoCrmMainBase::REDIRECT_URL]));
         $this->add(new Text('baseDomain'));
         $this->add(new Text('tokenForAmo'));
-
-        $useInterception = ['value' => null];
-        if ($entity->useInterception === '1') {
-            $useInterception = ['checked' => 'checked', 'value' => null];
-        }
-        $this->add(new Check('useInterception', $useInterception));
-
-        $panelIsEnable = ['value' => null];
-        if ($entity->panelIsEnable === '1') {
-            $panelIsEnable = ['checked' => 'checked', 'value' => null];
-        }
-        $this->add(new Check('panelIsEnable', $panelIsEnable));
-
-        $isPrivateWidget = ['value' => null];
-        if ($entity->isPrivateWidget === '1') {
-            $isPrivateWidget = ['checked' => 'checked', 'value' => null];
-        }
-        $this->add(new Check('isPrivateWidget', $isPrivateWidget));
+        $this->addCheckBox('useInterception', intval($entity->useInterception) === 1);
+        $this->addCheckBox('panelIsEnable', intval($entity->panelIsEnable) === 1);
+        $this->addCheckBox('isPrivateWidget', intval($entity->isPrivateWidget) === 1);
+        $this->addCheckBox('disableDetailedCdr', intval($entity->disableDetailedCdr) === 1);
         $this->add(new Text('privateClientId'));
         $this->add(new Text('privateClientSecret'));
-
-
-        $values = ['value' => null];
-        if ($entity->disableDetailedCdr === '1') {
-            $values = ['checked' => 'checked', 'value' => null];
-        }
-        $this->add(new Check('disableDetailedCdr', $values));
-
         $variants = [
             ModuleAmoCrm::RESP_TYPE_FIRST      => Util::translate('mod_amo_type_responsible_answered_first', false),
-            ModuleAmoCrm::RESP_TYPE_LAST       => Util::translate('mod_amo_type_responsible_answered_last',  false),
-            ModuleAmoCrm::RESP_TYPE_CONTACT    => Util::translate('mod_amo_type_responsible_contact',  false),
-            ModuleAmoCrm::RESP_TYPE_RULE       => Util::translate('mod_amo_type_responsible_rule',  false),
+            ModuleAmoCrm::RESP_TYPE_LAST       => Util::translate('mod_amo_type_responsible_answered_last', false),
+            ModuleAmoCrm::RESP_TYPE_CONTACT    => Util::translate('mod_amo_type_responsible_contact', false),
+            ModuleAmoCrm::RESP_TYPE_RULE       => Util::translate('mod_amo_type_responsible_rule', false),
         ];
         $type = new Select(
-            'respCallAnsweredHaveClient', $variants, [
+            'respCallAnsweredHaveClient',
+            $variants,
+            [
                              'useEmpty' => false,
                              'class' => 'ui selection dropdown search',
                          ]
@@ -72,11 +61,13 @@ class ModuleAmoCrmForm extends Form
 
         $variants = [
             ModuleAmoCrm::RESP_TYPE_FIRST      => Util::translate('mod_amo_type_responsible_answered_first', false),
-            ModuleAmoCrm::RESP_TYPE_LAST       => Util::translate('mod_amo_type_responsible_answered_last',  false),
-            ModuleAmoCrm::RESP_TYPE_RULE       => Util::translate('mod_amo_type_responsible_rule',  false),
+            ModuleAmoCrm::RESP_TYPE_LAST       => Util::translate('mod_amo_type_responsible_answered_last', false),
+            ModuleAmoCrm::RESP_TYPE_RULE       => Util::translate('mod_amo_type_responsible_rule', false),
         ];
         $type = new Select(
-            'respCallAnsweredNoClient', $variants, [
+            'respCallAnsweredNoClient',
+            $variants,
+            [
                                             'useEmpty' => false,
                                             'class' => 'ui selection dropdown search',
                                         ]
@@ -85,11 +76,13 @@ class ModuleAmoCrmForm extends Form
 
         $variants = [
             ModuleAmoCrm::RESP_TYPE_FIRST      => Util::translate('mod_amo_type_responsible_missed_first', false),
-            ModuleAmoCrm::RESP_TYPE_LAST       => Util::translate('mod_amo_type_responsible_missed_last',  false),
-            ModuleAmoCrm::RESP_TYPE_RULE       => Util::translate('mod_amo_type_responsible_rule',  false),
+            ModuleAmoCrm::RESP_TYPE_LAST       => Util::translate('mod_amo_type_responsible_missed_last', false),
+            ModuleAmoCrm::RESP_TYPE_RULE       => Util::translate('mod_amo_type_responsible_rule', false),
         ];
         $type = new Select(
-            'respCallMissedNoClient', $variants, [
+            'respCallMissedNoClient',
+            $variants,
+            [
                                             'useEmpty' => false,
                                             'class' => 'ui selection dropdown search',
                                         ]
@@ -98,16 +91,19 @@ class ModuleAmoCrmForm extends Form
 
         $variants = [
             ModuleAmoCrm::RESP_TYPE_FIRST      => Util::translate('mod_amo_type_responsible_missed_first', false),
-            ModuleAmoCrm::RESP_TYPE_LAST       => Util::translate('mod_amo_type_responsible_missed_last',  false),
-            ModuleAmoCrm::RESP_TYPE_CONTACT    => Util::translate('mod_amo_type_responsible_contact',  false),
-            ModuleAmoCrm::RESP_TYPE_RULE       => Util::translate('mod_amo_type_responsible_rule',  false),
+            ModuleAmoCrm::RESP_TYPE_LAST       => Util::translate('mod_amo_type_responsible_missed_last', false),
+            ModuleAmoCrm::RESP_TYPE_CONTACT    => Util::translate('mod_amo_type_responsible_contact', false),
+            ModuleAmoCrm::RESP_TYPE_RULE       => Util::translate('mod_amo_type_responsible_rule', false),
         ];
         $type = new Select(
-            'respCallMissedHaveClient', $variants, [
+            'respCallMissedHaveClient',
+            $variants,
+            [
                                             'useEmpty' => false,
                                             'class' => 'ui selection dropdown search',
                                         ]
         );
         $this->add($type);
     }
+
 }
